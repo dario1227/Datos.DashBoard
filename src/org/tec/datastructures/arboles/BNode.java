47f88b92d4850a3b7668e39757e80e81e68b50da
Dario <darior1227@gmail.com>
2017-11-14 21:44:44 -0600

    [arbol B y Splay]

---
+++ src/org/tec/datastructures/arboles/BNode.java
@@ -0,0 +1,30 @@
+package org.tec.datastructures.arboles;
+
+public class BNode {
+		int t; 
+		int count; 
+		int key[];  
+		BNode child[]; 
+		boolean leaf;
+		BNode parent;
+		public BNode(int t, BNode parent)
+		{
+			this.t = t; 
+			this.parent = parent;		
+			key = new int[2*t - 1]; 
+			child = new BNode[2*t]; 
+			leaf = true;
+			count = 0; 
+		}
+
+
+		public int getValue(int index)
+		{
+			return key[index];
+		}
+
+		public BNode getChild(int index)
+		{
+			return child[index];
+		}
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -11,11 +11,29 @@ public class Test {
 		int[]arr= {1,5,3,8,5,2,6};
 		BusquedaInterpolacion.start(arr,3);
 		print(arr);
+		System.out.println("\n"+suma(123457));
 	}
 	public static void print(int[]arr) {
 		for(int x:arr) {
 			System.out.print(x+",");
 		}
 	}
+	public static int suma(int x) {
+		return sumaAux(x,0) ;
 
+	}
+	private static int sumaAux(int x, int result) {
+		if(x!=0) {
+			int y=x%10;
+			if(y%2==1) {
+				result+=y;
+				return sumaAux(x/10,result);
+			}
+			else {
+				return sumaAux(x/10,result);
+			}
+		}
+		return result;
+	}
+	
 }
